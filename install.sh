2084a7175406510bb751e99f9e73d59a0b9a223e
Chad Glendenin <chad@glendenin.com>
2009-08-08 11:06:19 -0500

    simplest possible install script.

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+for i in .*
+do
+    src=$PWD/$i
+    dst=$HOME/$i
+    if [ -f $i ]
+    then
+        if [ -f $HOME/$i ]
+        then
+            echo "Skipping file '$dst'"
+        else
+            echo "Linkng '$src' to '$dst'"
+            ln -s "$src" "$dst"
+        fi
+    fi
+done
+

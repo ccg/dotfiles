8ee419c5efb2a5ed5e0e842241ad625ee9f24b21
Chad Glendenin <ccg@pybrew.com>
2009-10-17 02:42:16 +0000

    git-config script

---
+++ git.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git config --global color.ui "auto"
+git config --global color.diff "auto"
+git config --global color.status "auto"
+git config --global color.branch "auto"
+git config --global user.name "Chad Glendenin"
+git config --global user.email "ccg@pybrew.com"
+git config --global core.excludesfile "$HOME/.gitexcludes"
